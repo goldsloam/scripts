fd0db44bdd59324b3c8085c5fffdae86086fbacb
Andrew <goldsloam@gmail.com>
2018-06-01 10:41:39 -0700

    Update network.sh
    make wpa_sup run in the background.

--- network.sh
+++ network.sh
@@ -3,6 +3,6 @@
 # network solutions
 #
 sudo macchange -r wlp58s0
-sudo wpa_supplicant -i wlp58s0 -c /etc/wpa_supplicant/home.conf
+sudo wpa_supplicant -b -i wlp58s0 -c /etc/wpa_supplicant/home.conf
 sudo dhcpcd wlp58s0
 exit
